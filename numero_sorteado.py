fbe298d4dac26a7f20cfc95ac8e0cc605b801cc1
Jonas lima <>
2025-07-02 00:47:26 -0300

    crie um código para adinhar o numero sorteado

---
+++ numero_sorteado.py
@@ -0,0 +1,11 @@
+#Nesse código o usuário vai tentar acertar o número sorteado.
+
+numeroSorteado = 17
+numero_usuario = int(input("Digite um número:"))
+
+while numero_usuario != numeroSorteado:
+    print("Número errado, tente de novo!")
+    numero_usuario = int(input("Digite um número:"))
+
+else:
+    print("Parabéns, você acertou!!!")
